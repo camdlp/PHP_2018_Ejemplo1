326d3a344f967daf5f704103a793ee6fa2d7a834
carlosabia <carlosabia@10.10.29.125>
2018-09-24 19:50:38 +0200

    Añadido login.

--- index.php
+++ index.php
@@ -10,34 +10,57 @@ and open the template in the editor.
         <title>PRUEBA DE PHP CON BOOTSTRAP</title>
         <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
     </head>
-    <body>
-        <div class="container">            
+    <body style="background-color: gray">
+        <div class="container">       
+            <div class="row">
+                <div class="col-12">
+                    <h2 class="text-center" style="color: lightgrey">Ejemplo inicio sesión</h2>
+                </div>
+            </div>
             <div class="row">
                 <div class="col-4">
-                    <button type="button" id="boton1" class="btn btn-primary">Primary</button>
-                    
+
+
                 </div>
                 <div class="col-4">
-                    <input id="cajaNombre" name="usuario_nombre" type="text" placeholder="Usuario">
-                    
+                    <br/><br/>
+                    <form action="index2.php">
+                        <input id="cajaNombre" class="form-control" name="usuario_nombre" type="text" placeholder="Usuario" required="required">
+                        <br/>
+                        <input id="cajaPassword" class="form-control" type="password" placeholder="Contraseña">
+                        <br/>
+                        <button id="boton1" class="btn btn-primary btn-block" type="submit">Primary</button>
+                        <br/>
+                        <p>
+                            Year (yyyy-mm-dd):
+                            <input data-validation="date" data-validation-format="yyyy-mm-dd" class="form-control">
+                        </p>
+                    </form>
                 </div>
-                <div class="col-4"></div>
+                <div class="col-4">
+
+                </div>
             </div>
         </div>
-        <?php
-        // put your code here
-        ?>
+
+        <script src="js/jquery-3.3.1.min.js" type="text/javascript"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.3.26/jquery.form-validator.min.js"></script>
+        <script>
+
+            //document ready se ejecuta cuando toda la página se ha cargado correctamente.
+            $(document).ready(function () {
+                //$('#cajaNombre').hide();
+            });
+
+            $.validate({
+                lang: 'es'
+            });
+
+            $('#boton1').click(function () {
+                $('#cajaNombre').show(200);
+            });
+        </script>
+
     </body>
-    <script src="js/jquery-3.3.1.min.js" type="text/javascript"></script>
-    <script>
-        
-        //document ready se ejecuta cuando toda la página se ha cargado correctamente.
-        $(document).ready(function (){
-            $('#cajaNombre').hide();
-        });
-        
-        $('#boton1').click(function(){
-            $('#cajaNombre').show();
-        });
-    </script>
+
 </html>
