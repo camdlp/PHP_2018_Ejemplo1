7b15bcd0d9c0733483a9df74b124e230c56f7e38
carlosabia <carlosabia@10.10.29.125>
2018-09-27 18:06:21 +0200

    añadida base de datos

--- login.php
+++ login.php
@@ -1,10 +1,21 @@
 <?php
 
-include 'configuracion.php';
+include 'misFunciones.php';
 
+$mysqli = conectaBBDD();
 
 $cajaNombre = $_POST['cajaNombre'];
 
 $cajaPassword = $_POST['cajaPassword'];
 
-echo 'Has escrito el usuario '.$cajaNombre.' y la contraseña '.$cajaPassword;
+//echo 'Has escrito el usuario '.$cajaNombre.' y la contraseña '.$cajaPassword;
+
+$resultadoQuery = $mysqli ->query("SELECT * FROM preguntas");
+
+$numPreguntas = $resultadoQuery -> num_rows;
+
+for ($i = 0; $i < $numPreguntas; $i++){
+    $r = $resultadoQuery -> fetch_array();
+    echo $r['enunciado'].'<br/>';
+    
+}
