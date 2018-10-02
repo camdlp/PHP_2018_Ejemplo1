01c8fb00e5b02e46452d06630c205aed4f0675ac
carlosabia <carlosabia@10.10.29.125>
2018-10-02 19:23:33 +0200

    Añadido y protegido login. Falta encriptado.

---
+++ app.php
@@ -0,0 +1,9 @@
+<h1>BIENVENIDOS A LA APLICACIÓN DEL TRIVIAL DE LA EVAU</h1>
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
--- login.php
+++ login.php
@@ -2,20 +2,42 @@
 
 include 'misFunciones.php';
 
+
+function limpiarPalabra($palabra) {
+    //filtro muy básico para evitar la inyección SQL -> 'OR'1'='1
+    $palabra = trim($palabra, "'");
+    $palabra = trim($palabra, " ");
+    $palabra = trim($palabra, "-");
+    $palabra = trim($palabra, "´");
+    $palabra = trim($palabra, "`");
+    $palabra = trim($palabra, '"');
+    return $palabra;
+}
+
 $mysqli = conectaBBDD();
 
-$cajaNombre = $_POST['cajaNombre'];
+$cajaNombre = limpiarPalabra($_POST['cajaNombre']);
 
-$cajaPassword = $_POST['cajaPassword'];
+$cajaPassword = limpiarPalabra($_POST['cajaPassword']);
+
 
 //echo 'Has escrito el usuario '.$cajaNombre.' y la contraseña '.$cajaPassword;
 
-$resultadoQuery = $mysqli ->query("SELECT * FROM preguntas");
+$resultadoQuery = $mysqli->query("SELECT * FROM usuarios "
+        . "WHERE nombreUsuario = '$cajaNombre' AND userPass = '$cajaPassword'");
 
-$numPreguntas = $resultadoQuery -> num_rows;
+$numUsuarios = $resultadoQuery->num_rows;
 
-for ($i = 0; $i < $numPreguntas; $i++){
-    $r = $resultadoQuery -> fetch_array();
-    echo $r['enunciado'].'<br/>';
-    
+//for ($i = 0; $i < $numUsuarios; $i++){
+//    $r = $resultadoQuery -> fetch_array();
+//    echo $r['nombreUsuario'].'<br/>';
+//    
+//}
+
+if ($numUsuarios > 0) {
+    //muestro la pantalla de las aplcación
+    require 'app.php';
+} else {
+    //muestro una pantalla de error
+    require 'error.php';
 }
